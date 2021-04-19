43122b930b35add9e7d1ed16e136f4218b841dcb
liran <liranhal@campus.techniom.ac.il>
2021-04-19 20:03:31 +0300

    Removed generate_frames.py, merged with utils.py. Added function to utils.py that creates one YOLO label file for all images, from  directory of separate label files.

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import time
 
 
 class VideoHandler:
@@ -45,3 +46,38 @@ class VideoHandler:
                 frame_count += 1
 
         capture.release()
+
+
+def generate_frames(video_dir, save_dir):
+    for file_path in os.listdir(video_dir):
+        s = time.time()
+        name = os.path.splitext(file_path)[0]
+        vid = VideoHandler(os.path.join(video_dir, file_path), name=name)
+        save_path = os.path.join(save_dir, name)
+        os.mkdir(save_path)
+        vid.sample_from_vid(save_dir=save_path)
+        e = time.time()
+        print("completed video:", name, "in", e - s, "seconds")
+
+
+def generate_YOLO_labels(data_dir, labels_dir, save_path):
+    new_lines = []
+    for file_path in os.listdir(labels_dir):
+        with open(os.path.join(labels_dir, file_path), 'r') as label_f:
+            img_path = os.path.join(data_dir, os.path.splitext(file_path)[0]+".jpg")
+            annotations = [img_path]
+            for line in label_f:
+                class_label = line.split()[0]
+                coordinates = line.split()[1:]
+                object_annot = ','.join(coordinates) + ',' + class_label
+                annotations.append(object_annot)
+            new_lines.append(' '.join(annotations) + '\n')
+    with open(save_path, 'w') as out_file:
+        out_file.writelines(new_lines)
+
+
+if __name__ == '__main__':
+    data_dir = '/data/home/liranhal/code/data/data_APAS/cross_valid/images'
+    labels_dir = '/data/home/liranhal/code/data/data_APAS/cross_valid/labels_main_head'
+    save_path = '/data/home/liranhal/code/data/ASAP_main_annotations.txt'
+    generate_YOLO_labels(data_dir, labels_dir, save_path)
